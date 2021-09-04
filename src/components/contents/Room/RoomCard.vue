a813c8206a377aeb618d5efea2855e8c6ae615c3
Wony-Lee <a1_11_11@naver.com>
2021-09-04 16:42:45 +0900

    roomCard Update

---
+++ src/components/contents/Room/RoomCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="roomCardSection">
+        <div class="roomCard">
+            <!-- 방제목 및 인원수 -->
+            <div></div>
+            <!-- 내용, 재생중인 곡 및 썸네일 -->
+            <div>
+                <!-- 내용 -->
+                <div>
+                    힙합
+                </div>
+                <!-- 재생중인 곡 -->
+                <div>
+                    힙합곡
+                </div>
+                <!-- 썸네일 순서가 바뀔 수 있음 혹은 뺄 수 있음. -->
+                <div>
+                    힙합이미지
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.roomCardSection {
+    width: 100%;
+    margin: 10px;
+
+    & .roomCard {
+        width: 100%;
+        border: 1px solid red;
+    }
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,13 @@
-const routes = [];
+const routes = [
+    {
+        path: "/",
+        component: () => import("@/components/contents/MainContents.vue"),
+        // 이름을 가진 라우트 설정시
+        // name: main,
+        // components: {
+        //     name: () => import(),
+        // },
+    },
+];
 
 export default routes;
