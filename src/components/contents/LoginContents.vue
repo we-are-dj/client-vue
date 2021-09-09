fd0ce201304de85cafc3d7688c379415c0389903
Wony <68363082+Wony-Lee@users.noreply.github.com>
2021-09-10 02:24:07 +0900

    09-10/AM 2:24 작업하다 걍 잠

---
+++ src/components/contents/LoginContents.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="loginSection">
+    <div>로그인</div>
+    <div>
+      <!-- 카카오 로그인 넣을 예정 -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoed>
+.loginSection {
+  margin: 0 auto;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,21 @@
 const routes = [
-    {
-        path: "/",
-        component: () => import("@/components/contents/MainContents.vue"),
-        // 이름을 가진 라우트 설정시
-        // name: main,
-        // components: {
-        //     name: () => import(),
-        // },
-    },
+  {
+    path: "/",
+    component: () => import("@/components/contents/MainContents.vue"),
+    // 이름을 가진 라우트 설정시
+    // name: main,
+    // components: {
+    //     name: () => import(),
+    // },
+  },
+  {
+    path: "/login",
+    component: () => import("@/components/contents/LoginContents.vue"),
+  },
+  {
+    path: "/news",
+    component: () => import("@/components/contents/NewsContents.vue"),
+  },
 ];
 
 export default routes;
