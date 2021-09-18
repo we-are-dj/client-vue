215b610144d24cc0715f57d6e0450203c2c6ff71
D.seok <ehskstkrjsa@naver.com>
2021-09-18 15:26:02 +0900

    NavBar - scss추가, NavBar 레이아웃 잡기 시작

--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        Hello NavBar
+        <div class="nav">
+            <div class="logo">
+                <h1><a href="">Music</a></h1>
+            </div>
+            <ul>
+                <li><a href="">메뉴1</a></li>
+                <li><a href="">메뉴2</a></li>
+                <li><a href="">메뉴3</a></li>
+                <li><a href="">메뉴4</a></li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -8,4 +18,6 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import './NavBar.scss';
+</style>
