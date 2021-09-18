ca0573da63b3bd6787f7db3d4f160da59ce91898
Wony-Lee <a1_11_11@naver.com>
2021-09-18 13:49:27 +0900

    Layout Update Default

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <main-form>
+        <template v-slot:topLayout>Hello World 2</template>
+        <template v-slot:bodyLayout>Hello World 3</template>
+        <template v-slot:footerLayout>Hello World 4</template>
+    </main-form>
+</template>
+
+<script>
+import MainForm from "../layout/MainForm.vue";
+
+export default {
+    components: { MainForm },
+};
+</script>
+
+<style></style>
