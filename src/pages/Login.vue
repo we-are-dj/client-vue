6a11832a4ccd55fc81c59fbd3541c847ed19ed06
D.seok <ehskstkrjsa@naver.com>
2021-09-19 20:35:54 +0900

    로그인 페이지 생성 및 로그인 라우터 생성

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="logoBox">
+            <div class="logo">
+                <h1><a href="javascript:void(0)">로고</a></h1>
+            </div>
+        </div>
+        <div class="discBox">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,9 @@
-const routes = [];
+const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import("@/pages/Login.vue")
+    },
+];
 
 export default routes;
