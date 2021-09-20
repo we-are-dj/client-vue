f4ffa2ecdafa982ca7fffb9b6f4b20ac490b70f0
D.seok <ehskstkrjsa@naver.com>
2021-09-20 22:20:40 +0900

    NavBar - router-link추가

--- src/components/Nav/NavBar.vue
+++ src/components/Nav/NavBar.vue
@@ -2,10 +2,10 @@
     <div class="container">
         <div class="nav">
             <div class="logo">
-                <h1><a href="">Music</a></h1>
+                <h1><a href=""><router-link to="/">Music</router-link></a></h1>
             </div>
             <ul>
-                <li><a href="">메뉴1</a></li>
+                <li><a href=""><router-link to ="/login">메뉴1</router-link></a></li>
                 <li><a href="">메뉴2</a></li>
                 <li><a href="">메뉴3</a></li>
                 <li><a href="">메뉴4</a></li>
