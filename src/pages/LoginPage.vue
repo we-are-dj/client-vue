67afa587d3116defa552a196172779d2a849e70e
D.seok <ehskstkrjsa@naver.com>
2021-09-22 17:44:06 +0900

    LoginPage -> (임시) 메인으로 가기 버튼 생성

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -15,6 +15,7 @@
                 </div>
             </div>
             <div class="kakao">
+                <button @click="goMain">메인으로 가기</button>
                 <!-- <a id="custom-login-btn" @click="kakaoLogin()">
                     <img
                         src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
@@ -41,45 +42,48 @@ export default {
         // Kakao.isInitialized();
         // this.kakaoLogout();
     },
-//     methods: {
-//     kakaoLogin() {
-//       window.Kakao.Auth.login({
-//         scope: "profile",
-//         success: this.getKakaoAccount,
-//       });
-//         // 메인 페이지로 이동
-//         this.$router.push('/main');
-//     },
-//     getKakaoAccount() {
-//       window.Kakao.API.request({
-//         url: "/v2/user/me",
-//         success: (res) => {
-//           const kakao_account = res.kakao_account;
-//           const nickname = kakao_account.profile.nickname; //카카오 닉네임
-//           console.log("nickname", nickname);
-//           //로그인 처리 구현
-//           console.log(kakao_account);
-//           this.$store.commit("user", kakao_account);
-//           alert("로그인 성공!");
-//         },
-//         fail: (error) => {
-//           // this.$router.push("/errorPage");
-//           console.log(error);
-//         },
-//       });
-//     },
-//     kakaoLogout() {
-//       if (!window.Kakao.Auth.getAccessToken()) {
-//         console.log("Not logged in.");
-//         return;
-//       }
-//       window.Kakao.Auth.logout((response) => {
-//         //로그아웃
-//         console.log("access token:", window.Kakao.Auth.getAccessToken());
-//         console.log("log out:", response);
-//       });
-//     },
-//   },
+    methods: {
+        goMain(){
+            this.$router.push('/main')
+        }
+    // kakaoLogin() {
+    //   window.Kakao.Auth.login({
+    //     scope: "profile",
+    //     success: this.getKakaoAccount,
+    //   });
+    //     // 메인 페이지로 이동
+    //     this.$router.push('/main');
+    // },
+    // getKakaoAccount() {
+    //   window.Kakao.API.request({
+    //     url: "/v2/user/me",
+    //     success: (res) => {
+    //       const kakao_account = res.kakao_account;
+    //       const nickname = kakao_account.profile.nickname; //카카오 닉네임
+    //       console.log("nickname", nickname);
+    //       //로그인 처리 구현
+    //       console.log(kakao_account);
+    //       this.$store.commit("user", kakao_account);
+    //       alert("로그인 성공!");
+    //     },
+    //     fail: (error) => {
+    //       // this.$router.push("/errorPage");
+    //       console.log(error);
+    //     },
+    //   });
+    // },
+    // kakaoLogout() {
+    //   if (!window.Kakao.Auth.getAccessToken()) {
+    //     console.log("Not logged in.");
+    //     return;
+    //   }
+    //   window.Kakao.Auth.logout((response) => {
+    //     //로그아웃
+    //     console.log("access token:", window.Kakao.Auth.getAccessToken());
+    //     console.log("log out:", response);
+    //   });
+    // },
+  },
 }
 </script>
 
